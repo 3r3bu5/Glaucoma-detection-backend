7a09eb6247bf8af390b1618169b4faaf68c61031
Ashraf Gehad <deva4rf@gmail.com>
2021-03-15 14:29:20 +0200

    Style: Add eslint & prettier

--- src/server.js
+++ src/server.js
@@ -1,29 +1,25 @@
-var express = require("express");
+var express = require('express');
 var path = require('path');
 var app = express();
-require('dotenv').config()
+require('dotenv').config();
 
-var morgan = require("morgan");
-var helemt = require("helmet");
-var cors = require("cors");
+var morgan = require('morgan');
+var helemt = require('helmet');
+var cors = require('cors');
 
-/* 
-    Express middlewares
-*/
+/*
+ * Express middlewares
+ */
 app.use(express.json());
 app.use(helemt());
 app.use(cors());
-app.use(morgan('combined'))
-app.use("/static", express.static(path.join(__dirname, "public")));
+app.use(morgan('combined'));
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+  res.status(200).send({ status: 'Server is running!' });
+});
 
-
-app.get('/', (req,res) => {
-    res.status(200).send({status: "Server is running!"})
-})
-
-
-
-app.listen(process.env.PORT, (server) => {
-    console.log(`app is listening at http://localhost:${process.env.PORT}`)
-})
+app.listen(process.env.PORT || 3000, (server) => {
+  console.log(`app is listening at http://localhost:${process.env.PORT}`);
+});

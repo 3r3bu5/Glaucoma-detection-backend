461bcdadd15284e4097b022331425dd618bcdef6
Ashraf Gehad <deva4rf@gmail.com>
2021-03-17 01:24:49 +0200

    Feat: implement error handling
    Description: extend the built-in error object and add new attributes to work with logging service

---
+++ error/errorType.js
@@ -0,0 +1,3 @@
+module.exports = {
+  API_ENDPOINT_ERROR: 'API_ERROR',
+};
--- src/server.js
+++ src/server.js
@@ -2,10 +2,14 @@ var express = require('express');
 var path = require('path');
 var app = express();
 require('dotenv').config();
-
 var morgan = require('morgan');
 var helemt = require('helmet');
 var cors = require('cors');
+var loggerService = require('../services/logger.service');
+var APIError = require('../error/api.error');
+var errorStatusCode = require('../error/errorStatusCode');
+
+var logger = new loggerService('server');
 
 /*
  * Express middlewares
@@ -18,6 +22,33 @@ app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.status(200).send({ status: 'Server is running!' });
+});
+app.get('/favicon.ico', (req, res) => res.status(204));
+
+/*
+ * 404 ErrorHandler
+ */
+
+app.use((req, res, next) => {
+  const err = new APIError(
+    `404 NOT FOUND`,
+    errorStatusCode.NOT_FOUND,
+    ` "${req.originalUrl}" NOT FOUND `,
+    true
+  );
+  next(err);
+});
+
+app.use((error, req, res, next) => {
+  res.statusCode = error.httpStatusCode ? error.httpStatusCode : 500;
+  res.setHeader('Content-Type', 'application/json');
+  if (process.env.NODE_ENV === 'development') {
+    logger.error(error);
+    res.json(error);
+  } else {
+    logger.error(error);
+    res.json({ message: 'Error' });
+  }
 });
 
 app.listen(process.env.PORT || 3000, (server) => {

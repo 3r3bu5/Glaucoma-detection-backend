e7f9c387cf3db352204d05011dec1bff7078378f
Ashraf Gehad <deva4rf@gmail.com>
2021-04-24 20:57:57 +0200

    update: update new scan endpoint to throw error if user dosen't have credits

--- controller/scanCtrl.js
+++ controller/scanCtrl.js
@@ -8,14 +8,27 @@ var logger = new loggerService('scan.controller');
 // audit
 const { handleAuditing } = require('../audit/audit');
 const actionTypes = require('../audit/actionTypes');
+// error handling
+const APIError = require('../error/api.error');
+const ErrorStatus = require('../error/errorStatusCode');
+const ErrorType = require('../error/errorType');
 
 exports.newScan = async (req, res) => {
-  uploadSingle(req, res, async function (err) {
-    if (err) {
-      logger.error(`SCAN: error uploading or saving image`, err.message);
-      return res.status(500).send({ success: false, err: err.message });
-    } else {
-      try {
+  try {
+    var user = await User.findById(req.user._id);
+    if (user.credits < 1) {
+      throw new APIError(
+        ErrorType.API_ENDPOINT_ERROR,
+        ErrorStatus.FORBIDDEN,
+        'No enough credits',
+        true
+      );
+    }
+    uploadSingle(req, res, async function (err) {
+      if (err) {
+        logger.error(`SCAN: error uploading or saving image`, err.message);
+        return res.status(500).send({ success: false, err: err.message });
+      } else {
         var items = Array(0, 1);
         var result = items[Math.floor(Math.random() * items.length)];
         const newScan = new Scan({
@@ -27,11 +40,11 @@ exports.newScan = async (req, res) => {
             req.protocol + '://' + process.env.HOSTNAME + '/' + req.file.path,
         });
         const savedScan = await newScan.save();
-        var user = await User.findById(req.user._id);
         user.credits = user.credits - 1;
         const updatedUser = await user.save();
         logger.info(
-          `SCAN: created a new scan record for patient ${req.body._patientId}`);
+          `SCAN: created a new scan record for patient ${req.body._patientId}`
+        );
         handleAuditing(
           actionTypes.CREATE_NEW_SCAN,
           savedScan,
@@ -45,16 +58,16 @@ exports.newScan = async (req, res) => {
           result: savedScan.result,
           remCredits: updatedUser.credits,
         });
-      } catch (err) {
-        logger.error(
-          `SCAN: Error creating scan record for patient ${req.body._patientId} `,
-          err.message
-        );
-        res.setHeader('Content-Type', 'application/json');
-        return res
-          .status(err.httpStatusCode || 500)
-          .json({ success: false, err: err.description || err.message });
       }
-    }
-  });
+    });
+  } catch (err) {
+    logger.error(
+      `SCAN: Error creating scan record for patient ${req.body._patientId} `,
+      err.message
+    );
+    res.setHeader('Content-Type', 'application/json');
+    return res
+      .status(err.httpStatusCode || 500)
+      .json({ success: false, err: err.description || err.message });
+  }
 };

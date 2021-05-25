d53c0e991724dfee50502e984cb2a1fbb03c3af9
Ashraf Gehad <deva4rf@gmail.com>
2021-05-25 12:42:53 +0200

    update: update user's verified property default value to false && update email transporter

--- utils/sendEmail.utils.js
+++ utils/sendEmail.utils.js
@@ -1,26 +1,27 @@
 require('dotenv').config();
 const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
 
-var smtpTransport = nodemailer.createTransport(
-  `smtps://${process.env.SMTP_USER}:` +
-    encodeURIComponent(process.env.SMTP_PASS) +
-    `@smtp.gmail.com:465`
-);
+var smtpTransport = nodemailer.createTransport(sendgridTransport({
+  auth: {
+    api_key: process.env.SENDGRID_KEY
+  }
+}))
 
 module.exports = async (req, user, token) => {
   var mailOptions = {
     from: process.env.SMTP_USER,
     to: user.email,
-    subject: 'Account Verification Link',
+    subject: 'Glaucoma Lab - Account Verification Link',
     text:
       'Hello ' +
       user.fname +
       ' ' +
       user.lname +
       ',\n\n' +
-      'Please verify your account by clicking the link: \nhttp://' +
-      req.headers.host +
-      '/user/verify/' +
+      'Please verify your account by clicking the link: \n' +
+      process.env.CLIENT_URL +
+      '/verification/' +
       user.email +
       '/' +
       token +

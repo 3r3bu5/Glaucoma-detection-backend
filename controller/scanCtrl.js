eb0c49e1c577c0fb2d3af0f1a595973d15ed6b1e
Ashraf Gehad <deva4rf@gmail.com>
2021-06-22 22:25:43 +0200

    feat: add predict model

--- controller/scanCtrl.js
+++ controller/scanCtrl.js
@@ -12,6 +12,7 @@ const actionTypes = require('../audit/actionTypes');
 const APIError = require('../error/api.error');
 const ErrorStatus = require('../error/errorStatusCode');
 const ErrorType = require('../error/errorType');
+var spawn = require("child_process").spawn;
 
 exports.newScan = async (req, res) => {
   try {
@@ -29,34 +30,37 @@ exports.newScan = async (req, res) => {
         logger.error(`SCAN: error uploading or saving image`, err.message);
         return res.status(500).send({ success: false, err: err.message });
       } else {
-        var items = Array(0, 1);
-        var result = items[Math.floor(Math.random() * items.length)];
-        const newScan = new Scan({
-          _patientId: req.body._patientId,
-          eye: req.body.eye,
-          notes: req.body.notes ? req.body.notes : '',
-          result: result,
-          Imagelink:
-            req.protocol + '://' + process.env.HOSTNAME + '/' + req.file.path,
-        });
-        const savedScan = await newScan.save();
-        user.credits = user.credits - 1;
-        const updatedUser = await user.save();
-        logger.info(
-          `SCAN: created a new scan record for patient ${req.body._patientId}`
-        );
-        handleAuditing(
-          actionTypes.CREATE_NEW_SCAN,
-          savedScan,
-          200,
-          null,
-          req.user._id
-        );
-        return res.status(200).json({
-          status: true,
-          filename: req.file.originalname,
-          result: savedScan.result,
-          remCredits: updatedUser.credits,
+        var process = spawn('python3', ["/mnt/d/Development/Projects/graduation/src/glac-server/model_func/prediction.py",
+          `/mnt/d/Development/Projects/graduation/src/glac-server/${req.file.path}`,
+        ]);
+        process.stdout.on('data', async (data) => {
+          const newScan = new Scan({
+            _patientId: req.body._patientId,
+            eye: req.body.eye,
+            notes: req.body.notes ? req.body.notes : '',
+            result: data,
+            Imagelink:
+              req.protocol + '://' + "localhost" + '/' + req.file.path,
+          });
+          const savedScan = await newScan.save();
+          user.credits = user.credits - 1;
+          const updatedUser = await user.save();
+          logger.info(
+            `SCAN: created a new scan record for patient ${req.body._patientId}`
+          );
+          handleAuditing(
+            actionTypes.CREATE_NEW_SCAN,
+            savedScan,
+            200,
+            null,
+            req.user._id
+          );
+          return res.status(200).json({
+            status: true,
+            filename: req.file.originalname,
+            result: savedScan.result,
+            remCredits: updatedUser.credits,
+          });
         });
       }
     });
